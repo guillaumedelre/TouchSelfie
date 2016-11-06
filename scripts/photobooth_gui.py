aa43d1f1845df7c9a8204b625b8bf733431ef85a
Guillaume Delré <delre.guillaume@gmail.com>
2016-11-06 20:34:39 +0100

    update menu

--- scripts/photobooth_gui.py
+++ scripts/photobooth_gui.py
@@ -299,24 +299,18 @@ root.geometry("%dx%d+0+0" % (WIDTH, HEIGHT))
 root.focus_set() # <-- move focus to this widget
 frame = Frame(root)
 
-def makeCommandMenu():
-    CmdBtn = Menubutton(root, text='Button Commands', underline=0)
-    CmdBtn.pack(side=LEFT, padx="2m")
-    CmdBtn.menu = Menu(CmdBtn)
-
 def makemenu(win):
     top = Menu(win)
     win.config(menu=top)
 
     snapMenu = Menu(top)
-    snapMenu.add_command(label='Selfie',  command=force_snap,  underline=0)
-    snapMenu.add_command(label='Warhol', command=warhol_snap,  underline=0)
-    snapMenu.add_command(label='x4', command=four_snap,  underline=0)
+    snapMenu.add_command(label='Selfie',  command=force_snap, font=('Tempus Sans ITC', 14), underline=0)
+    snapMenu.add_command(label='Warhol', command=warhol_snap, font=('Tempus Sans ITC', 14), underline=0)
+    snapMenu.add_command(label='x4', command=four_snap, font=('Tempus Sans ITC', 14), underline=0)
     snapMenu.add_separator()
     snapMenu.add_command(label='Quit',    command=win.quit, underline=0)
     top.add_cascade(label='Capture',     menu=snapMenu,        underline=0)
 
-makeCommandMenu()
 makemenu(root)
 
 # Button(frame, text="Exit", command=on_close).pack(side=LEFT)
