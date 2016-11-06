ac3f98854de21092e14f9194783697cde68157db
Guillaume Delré <delre.guillaume@gmail.com>
2016-11-06 21:23:34 +0100

    test preview_window size

--- scripts/photobooth_gui.py
+++ scripts/photobooth_gui.py
@@ -395,9 +395,7 @@ else:
 #    etext.config(state=DISABLED)
 
 def stand_by():
-    camera.preview_alpha = 192
-    camera.preview_window = (100, 100, SCREEN_W, SCREEN_H)
-    camera.start_preview()
+    camera.start_preview(fullscreen=False, crop=(0, 20, SCREEN_W, SCREEN_H), window=(0, 0, SCREEN_W, SCREEN_H), hflip=True, alpha=255)
     can.delete("text")
     can.create_text(WIDTH/2, HEIGHT/2, text="Touchez moi...", font=('Tempus Sans ITC', 18), tags="splash")
     can.update()
