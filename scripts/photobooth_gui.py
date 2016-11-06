6e272ce22759eb280fa3f350e3829a446d5623c6
Guillaume Delré <delre.guillaume@gmail.com>
2016-11-06 22:11:36 +0100

    test menu

--- scripts/photobooth_gui.py
+++ scripts/photobooth_gui.py
@@ -305,9 +305,9 @@ frame = Frame(root)
 def makemenu():
     top = Menu(root)
     root.config(menu=top)
-    top.add_command(label='Selfie    ', command=force_snap, font=('Tempus Sans ITC', 36))
-    top.add_command(label='  Warhol  ', command=warhol_snap, font=('Tempus Sans ITC', 36))
-    top.add_command(label='  x4  ', command=four_snap, font=('Tempus Sans ITC', 36))
+    top.add_command(label='Selfie  ', command=force_snap, font=('Tempus Sans ITC', 36))
+    top.add_command(label='   Warhol  ', command=warhol_snap, font=('Tempus Sans ITC', 36))
+    top.add_command(label='   x4  ', command=four_snap, font=('Tempus Sans ITC', 36))
     top.add_command(label='  Quitter', command=root.quit, font=('Tempus Sans ITC', 36))
 
     #paramMenu = Menu(top)
